b740e86e61d77cd2022edb91db5834b116bf6419
Austin Rebelo <000917638@mohawkcollege.ca>
2024-10-10 17:10:20 -0400

    Add arrays and rndom colours

--- Rebelo-Austin-a2-project/Game.cs
+++ Rebelo-Austin-a2-project/Game.cs
@@ -11,7 +11,8 @@ namespace Game10003
     public class Game
     {
         // Place your variables here:
-
+        bool isSheepNormal = true;
+        Color[] randomWool = new Color[4];
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
@@ -27,48 +28,67 @@ namespace Game10003
         /// </summary>
         public void Update()
         {
+            // Colours in order are white, black, beige, dark brown
+            Color[] sheepColors = [Color.White, Color.Black, new Color(184, 159, 137), new Color(80, 62, 62)];
+            
+
             Window.ClearBackground(Color.Cyan);
+            // Change colors in array
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Space))
+            {
+                for (int i = 0; i < randomWool.Length; i++)
+                {
+                    randomWool[i] = Random.Color();
+                }
+                isSheepNormal = false;
+            }
+            // Draw grass
             Draw.FillColor = Color.Green;
-            Draw.Rectangle(0 - 1, 400, 602, 201);
-            drawSheep(100, 150);
+            Draw.Rectangle(0, 400, 600, 200);
+            // Check sheep for normal colours to draw normal or random
+            if (isSheepNormal)
+            {
+                DrawSheep(100, 150, sheepColors);
+            }
+            else
+            {
+                DrawSheep(100, 150, randomWool);
+            }
         }
 
         // Draw sheep legs
-        void drawLegs(float x, float y)
-        {
-            Color whiteToRandom = Color.White;
+        void DrawLegs(float x, float y, Color[] sheepColors)
+        { 
             Draw.LineSize = 0;
-            Draw.FillColor = whiteToRandom;
+            Draw.FillColor = sheepColors[0];
             Draw.Rectangle(x, y, 65, 150);
-            Color feet = new Color(80, 62, 62);
-            Draw.FillColor = feet;
+            Draw.FillColor = sheepColors[3];
             Draw.Rectangle(x, y + 140, 65, 10);
         }
 
 
         // Function to call a sheep drawing
-        void drawSheep(float x, float y)
+        void DrawSheep(float x, float y,Color[] sheepColors)
         // Draw sheep
         {
-            Color whiteToRandom = Color.White;
             Draw.LineSize = 0;
             // Head
-            Draw.FillColor = whiteToRandom;
+            Draw.FillColor = sheepColors[0];
             Draw.Square(x, y, 100);
             Draw.LineSize = 0;
             // Face
-            Color Beige = new Color(184, 159, 137);
-            Draw.FillColor = Beige;
+            Draw.FillColor = sheepColors[2];
             Draw.Rectangle(x - 5, y + 10, 20, 80);
             // Eye
-            Draw.FillColor = Color.Black;
+            Draw.FillColor = sheepColors[1];
             Draw.Square(x - 5, y + 30, 10);
-            Draw.FillColor= whiteToRandom;
+            Draw.FillColor = sheepColors[0];
             Draw.Square(x, y + 30, 10);
             // Body
+            Draw.FillColor = sheepColors[0];
             Draw.Rectangle(x + 50, y + 50, 350, 175);
-            drawLegs(x + 60, y + 175);
-            drawLegs(x + 325, y + 175);
+            DrawLegs(x + 60, y + 175, sheepColors);
+            DrawLegs(x + 325, y + 175, sheepColors);
         }
     }
 }
